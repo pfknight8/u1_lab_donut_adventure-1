6554d63ab5bd13d7ec726caf9d16ca8151378e75
Patrick Knight <pfknight2@gmail.com>
2022-06-30 16:08:51 -0400

    Done, with 1 bonus

--- adventure.js
+++ adventure.js
@@ -11,20 +11,29 @@ class Hero {
   }
   talkSass() {
     let i = Math.floor(Math.random()*this.catchPhrases.length);
-    console.log(i);
+    // console.log(i);
     return this.catchPhrases[i];
   }
   announceHealth() {
-    console.log(this.health);
+    console.log(this.name + " has " + this.health + " points of health remaining.");
   }
-  battle() {
+  battle(target) {
     console.log('i\'m ready to rumble')
+    const weaponsList = Object.keys(this.inventory);
+    let i = Math.floor(Math.random() * weaponsList.length);
+    let weaponChosen = weaponsList[i];
+    console.log(this.name + " used " + weaponChosen + "!");
+    let damageDealt = this.inventory[weaponsList[i]];
+    console.log(target.name + " received " + damageDealt + " points of damage!");
+    target.health -= damageDealt;
+    target.announceHealth();
   }
 }
 
 const dougieDonut = new Hero("Dougie the Donut")
-console.log(dougieDonut.talkSass());
-dougieDonut.announceHealth();
+// console.log(dougieDonut.talkSass());
+// dougieDonut.announceHealth();
+
 
 class Enemy {
   constructor(name) {
@@ -39,16 +48,37 @@ class Enemy {
   }
   talkSmack() {
     let i = Math.floor(Math.random()*this.catchPhrases.length);
-    console.log(i);
+    // console.log(i);
     return this.catchPhrases[i];
   }
   announceHealth() {
-    console.log(this.health);
+    console.log(this.name + " has " + this.health + " points of health remaining.");
   }
-  battle() {
+  battle(target) {
     console.log('i\'m gonna flatten you like a slice of pepperoni!')
+    const weaponsList = Object.keys(this.inventory);
+    let i = Math.floor(Math.random() * weaponsList.length);
+    let weaponChosen = weaponsList[i];
+    console.log(this.name + " used " + weaponChosen + "!");
+    let damageDealt = this.inventory[weaponsList[i]]
+    console.log(target.name + " received " + damageDealt + " points of damage!");
+    target.health -= damageDealt;
+    target.announceHealth();
   }
 }
 
 const pizzaRat = new Enemy("Pizza Rat");
+// console.log(pizzaRat.talkSmack());
+
+//Dougie talk Sass
+console.log(dougieDonut.talkSass());
+//Pizza Rat talk Smack
 console.log(pizzaRat.talkSmack());
+//Dougie health
+dougieDonut.announceHealth();
+//Pizza Pat health
+pizzaRat.announceHealth();
+
+//Let us battle...
+dougieDonut.battle(pizzaRat);
+pizzaRat.battle(dougieDonut);
